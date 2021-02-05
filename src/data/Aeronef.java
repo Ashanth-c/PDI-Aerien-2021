0b92f37be94f4a56c91e8df3a98f91a99af58fe7
Ayadi Khadija <62354261+AyadiKhadija@users.noreply.github.com>
2021-02-05 21:36:25 +0100

    Update Aeronef.java

--- src/data/Aeronef.java
+++ src/data/Aeronef.java
@@ -11,13 +11,13 @@ public class Aeronef extends Block {
 	private int altitude;
 	private int speed;
 
-	public Aeronef(int startPoint, int endPoint) {
-		super(startPoint, endPoint);
+	public Aeronef(int startPoint, int endPoint, int currentPoint) {
+		super(startPoint, endPoint, currentPoint);
 	}
 
-	public Aeronef(int startPoint, int endPoint, String type, String model, String destination, String departure,
+	public Aeronef(int startPoint, int endPoint, int currentPoint, String type, String model, String destination, String departure,
 			int totalSeats, int fuel, int altitude, int speed) {
-		super(startPoint, endPoint);
+		super(startPoint, endPoint, currentPoint);
 		this.type = type;
 		this.model = model;
 		this.destination = destination;
