bfe9aac57514f7bce35f00144e4650dcc81d7e3d
Ayadi Khadija <62354261+AyadiKhadija@users.noreply.github.com>
2021-02-05 21:31:59 +0100

    Update Block.java

--- src/data/Block.java
+++ src/data/Block.java
@@ -3,11 +3,13 @@ package data;
 public class Block {
 	private int startPoint;
 	private int endPoint;
+	private int currentPoint;
 
-	public Block(int startPoint, int endPoint) {
+	public Block(int startPoint, int endPoint, int currentPoint) {
 		super();
 		this.startPoint = startPoint;
 		this.endPoint = endPoint;
+		this.currentPoint = currentPoint;
 	}
 
 	public int getStartPoint() {
@@ -25,9 +27,17 @@ public class Block {
 	public void setEndPoint(int endPoint) {
 		this.endPoint = endPoint;
 	}
+	
+	public int getCurrentPoint() {
+		return currentPoint;
+	}
+	public void setCurrentPoint(int currentPoint) {
+		this.currentPoint = currentPoint;
+	}
+
 
 	@Override
 	public String toString() {
-		return "Block [startPoint=" + startPoint + ", endPoint=" + endPoint + "]";
+		return "Block [startPoint=" + startPoint + ", endPoint=" + endPoint +",currentPoint=" + currentPoint + "]";
 	}
 }
