73cc43a50fbf2a31459da21421885406202f5730
Ayadi Khadija <62354261+AyadiKhadija@users.noreply.github.com>
2021-02-07 19:28:42 +0100

    Update FlockBirds.java

--- src/data/FlockBirds.java
+++ src/data/FlockBirds.java
@@ -4,18 +4,18 @@ public class FlockBirds extends Obstacle {
 	private int numberOfBirds;
 	private int speed;
 
-	public FlockBirds(int startPoint, int endPoint) {
-		super(startPoint, endPoint);
+	public FlockBirds(int abscisse, int ordonnee) {
+		super(abscisse, ordonnee);
 	}
 
-	public FlockBirds(int startPoint, int endPoint, int numberOfBirds, int speed) {
-		super(startPoint, endPoint);
+	public FlockBirds(int abscisse, int ordonnee, int numberOfBirds, int speed) {
+		super(abscisse, ordonnee);
 		this.numberOfBirds = numberOfBirds;
 		this.speed = speed;
 	}
 
-	public FlockBirds(int startPoint, int endPoint, String name, int altitude, int numberOfBirds, int speed) {
-		super(startPoint, endPoint, name, altitude);
+	public FlockBirds(int abscisse, int ordonnee, String name, int altitude, int numberOfBirds, int speed) {
+		super(abscisse, ordonnee, name, altitude);
 		this.numberOfBirds = numberOfBirds;
 		this.speed = speed;
 	}
