e17b8245abdf22a31f1eefd2fb130bc3342e7178
Khadija <62354261+AyadiKhadija@users.noreply.github.com>
2021-02-12 22:40:32 +0100

    Update SimulPara.java

--- src/gui/SimulPara.java
+++ src/gui/SimulPara.java
@@ -2,22 +2,24 @@
 
 package gui;
 
-//Khadija(pas sûr)
+//Khadija(pas trop sûr des valeurs)
 public class SimulPara {
 	
 	
 	
-	public static final int LINE_TOTAL_LENGTH = 1700;
+	public static final int LINE_TOTAL_LENGTH = 70000;
 	
-	public static final int BLOCK_LENGTH = 100;
+	public static final int BLOCK_LENGTH = 1000;
 	
 	
-	public static final int STATION_MIN_STAY = 2;
-	public static final int STATION_MAX_STAY = 6;
+	public static final int Airport_MIN_STAY = 2;
+	public static final int Airport_MAX_STAY = 6;
 		
 	public static final int Aeronef_INTERVALL = 50;
 	public static final int Aeronef_MIN_SPEED = 4000;
 	public static final int Aeronef_MAX_SPEED = 11200;
+	
+}
 	
 
 	
