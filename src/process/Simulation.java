1201dcb800f9e411c9215bd8f4f15b41d214ea8b
Khadija <62354261+AyadiKhadija@users.noreply.github.com>
2021-02-12 23:31:48 +0100

    Update and rename Simulation to Simulation.java

---
+++ src/process/Simulation.java
@@ -0,0 +1,32 @@
+
+package process;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import data.Block;
+import data.Line;
+import data.Aeronef;
+import gui.SimulPara;
+
+
+public class Simulation {
+	private Line line;
+	private volatile ArrayList<BlockManager> blockManagers = new ArrayList<BlockManager>();
+	private volatile HashMap<Integer, BlockManager> blockManagersByPosition = new HashMap<Integer, BlockManager>();
+
+	private ArrayList<AeronefManager> AeronefManagers = new ArrayList<AeronefManager>();
+
+	public Simulation() {
+		LineBuilder lineBuilder = new LineBuilder();
+		lineBuilder.buildLine(SimulPara.LINE_TOTAL_LENGTH, SimulPara.BLOCK_LENGTH);
+		line = lineBuilder.getBuiltLine();
+  }
+
+
+public Line getLine() {
+		return line;
+	}
+  
+}
+
