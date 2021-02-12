cb5d41170eb0023126673d952d67ea609ad8be70
Khadija <62354261+AyadiKhadija@users.noreply.github.com>
2021-02-12 22:39:00 +0100

    Create SimulPara.java

---
+++ src/gui/SimulPara.java
@@ -0,0 +1,23 @@
+
+
+package gui;
+
+//Khadija(pas sûr)
+public class SimulPara {
+	
+	
+	
+	public static final int LINE_TOTAL_LENGTH = 1700;
+	
+	public static final int BLOCK_LENGTH = 100;
+	
+	
+	public static final int STATION_MIN_STAY = 2;
+	public static final int STATION_MAX_STAY = 6;
+		
+	public static final int Aeronef_INTERVALL = 50;
+	public static final int Aeronef_MIN_SPEED = 4000;
+	public static final int Aeronef_MAX_SPEED = 11200;
+	
+
+	
