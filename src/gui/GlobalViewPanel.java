5f518bff2a96fd6bc7ad4167ef24a8230aadfb41
Ashanth <ashanth92@gmail.com>
2021-02-21 20:46:34 +0100

    add world map

--- src/gui/GlobalViewPanel.java
+++ src/gui/GlobalViewPanel.java
@@ -28,6 +28,7 @@ public class GlobalViewPanel extends JPanel {
 		Graphics2D g2 = (Graphics2D) g;
 		initLine(g2);
 		initAirport(g2);
+		printWorldMap(g2);
 	}
 
 	public void initAirport(Graphics2D g2) {
@@ -37,14 +38,14 @@ public class GlobalViewPanel extends JPanel {
 		printAirport(g2, 90, 100);
 		printAirport(g2, 1200, 50);
 		printAirport(g2, 700, 450);
-		printAirport(g2, 1000, 300);
-		printAirport(g2, 760, 150);
+		printAirport(g2, 902, 421);
+		printAirport(g2, 645, 240);
 
 	}
 	
 	
 	public void initLine(Graphics2D g2) {
-		printLine(g2,260+15,300+40,500+15,500+40);
+		printLine(g2,645+15,240+40,902+15,421+40);
 	}
 
 	private void printLine(Graphics2D g2, int x1, int y1, int x2, int y2) {
@@ -57,5 +58,10 @@ public class GlobalViewPanel extends JPanel {
 		g2.setColor(Color.BLACK);
 		g2.setStroke(new BasicStroke(6));
 		g2.drawImage(Utility.readImage("src/images/airport.png"), abscisse, ordonate, 30, 40, this);
+	}
+	
+	public void printWorldMap(Graphics2D g2) {
+//		System.out.println(this.getWidth());
+		g2.drawImage(Utility.readImage("src/images/world_map.png"), 100, 50, 1175, 699, this);
 	}
 }
