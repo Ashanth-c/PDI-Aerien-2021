d7006f8d6be05f7f17c05affcc90ceb2fc1683a7
Khadija <62354261+AyadiKhadija@users.noreply.github.com>
2021-04-03 21:10:00 +0200

    Update PaintStrategy.java

--- src/gui/PaintStrategy.java
+++ src/gui/PaintStrategy.java
@@ -5,7 +5,15 @@ import java.awt.Color;
 import java.awt.Graphics2D;
 
 import process.Utility;
-
+/*!
+ * @file PaintStrategy.java
+ * @brief la strategie du trace ou pienture
+ * @author Ashanth
+ * @author Maeva
+ * @author Khadija
+ * @version 1.0
+ * @date 06/04/2021
+ */
 public class PaintStrategy {
 	
 }
