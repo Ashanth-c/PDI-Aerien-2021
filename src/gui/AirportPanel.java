372bcd01392d0706219fbd609e09e9608d37c636
Khadija <62354261+AyadiKhadija@users.noreply.github.com>
2021-04-03 19:56:31 +0200

    Update AirportPanel.java

--- src/gui/AirportPanel.java
+++ src/gui/AirportPanel.java
@@ -1,6 +1,15 @@
 package gui;
 
 import javax.swing.JPanel;
+/*!
+ * @file AirportPanel.java
+ * @brief Classe representant le panel de l'aeroport
+ * @author Ashanth
+ * @author Maeva
+ * @author Khadija
+ * @version 1.0
+ * @date 06/04/2021
+ */
 
 public class AirportPanel extends JPanel{
 
