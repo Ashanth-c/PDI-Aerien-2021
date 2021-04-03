f3edd824cbc3145af8a99085f17ed38d52969da5
Khadija <62354261+AyadiKhadija@users.noreply.github.com>
2021-04-03 11:34:17 +0200

    commentaire_doxygen

--- src/data/FlockBirds.java
+++ src/data/FlockBirds.java
@@ -1,57 +1,44 @@
 package data;
-/**
- * 
- * @author Maeva
- *
- * Classe décrivant un groupe d'oiseau
- * Classe définie par le nombre d'oiseaux par groupe, leur vitesse, leur radius et l'angle de leur trajectoire
- *
+
+/*!
+ * @file FlockBirds.java
+ * @brief Classe representant un groupe d'oiseau
  * Cette classe est une classe fille d'Obstacle
+ * @author Ashanth
+ * @author Maeva
+ * @author Khadija
+ * @version 1.0
+ * @date 06/04/2021
  */
 public class FlockBirds extends Obstacle {
 	private int numberOfBirds;
 	private int speed;
 	private double radius;
 	private double teta=0;
-/**
- * Constructeur FlockBirds	
+/*!
+ * FlockBirds()	
  * @param abscisse
  * @param ordonnee
  */
-	/*
-	 * Constructeur FlockBirds
-	 * param:
-	 * 		abscisse : int
-	 * 		ordonnee : int
-	 * 
-	 * 
-	 */
+	
 	public FlockBirds(int abscisse, int ordonnee) {
 		super(abscisse, ordonnee);
 	}
 /**
- * Constructeur FlockBirds
+ * FlockBirds()
  * @param abscisse
  * @param ordonnee
  * @param numberOfBirds
  * @param speed
  */
-	/*
-	 * Constructeur FlockBirds
-	 * 
-	 *param:
-	 *		abscisse : int
-	 *		ordonnee : int
-	 *		numberOfBirds : int
-	 *		speed : int
-	 */
+	
 	public FlockBirds(int abscisse, int ordonnee, int numberOfBirds, int speed) {
 		super(abscisse, ordonnee);
 		this.numberOfBirds = numberOfBirds;
 		this.speed = speed;
 	}
-/**
- * Constructeur FlockBirds
+/*!
+ * FlockBirds()
  * @param abscisse
  * @param ordonnee
  * @param name
@@ -59,121 +46,75 @@ public class FlockBirds extends Obstacle {
  * @param numberOfBirds
  * @param speed
  */
-	/*
-	 * Constructeur FlockBirds
-	 * 
-	 * param:
-	 * 		abscisse, ordonnee : int
-	 * 		name : String
-	 * 		altitude, numberofBirds, speed : int
-	 */
 	public FlockBirds(int abscisse, int ordonnee, String name, int altitude, int numberOfBirds, int speed) {
 		super(abscisse, ordonnee, name, altitude);
 		this.numberOfBirds = numberOfBirds;
 		this.speed = speed;
 	}
-/**
- * getter getNumberofBirds
+/*!
+ * getNumberofBirds()
+ * @brief retourne le nombre de groupe des oiseaux
  * @return numberOfBirds
  */
-	/*
-	 * getter getNumberofBirds
-	 * 
-	 * return numberOfBirds
-	 */
 	public int getNumberOfBirds() {
 		return numberOfBirds;
 	}
-/**
- * setter setNumberofBirds
+/*!
+ * setNumberofBirds()
  * @param numberOfBirds
  */
-	/*
-	 * setter setNumberOfBirds
-	 * param:
-	 * 		numberOfBirds : int
-	 */
 	public void setNumberOfBirds(int numberOfBirds) {
 		this.numberOfBirds = numberOfBirds;
 	}
-/**
- * getter getSpeed
+/*!
+ * getSpeed()
+ * @brief retourne la vitesse des oiseaux
  * @return speed
  */
-	/*
-	 * getter getSpeed
-	 * 
-	 * return speed, int
-	 */
 	public int getSpeed() {
 		return speed;
 	}
-/**
- * setter setSpeed
+/*!
+ * setSpeed()
  * @param speed
  */
-	/*
-	 * setter setSpeed
-	 * param :
-	 * 		speed : int
-	 */
 	public void setSpeed(int speed) {
 		this.speed = speed;
 	}
-/**
- * toString method
+/*!
+ * toString()
+ * @return une chaine de caractÃ¨re qui affiche le nom des groupes des oiseaux
  */
-	/*
-	 * toString method
-	 */
 	@Override
 	public String toString() {
 		return "FlockBirds " + getName();
 	}
-/**
- * getter getRadius
+/*!
+ * getRadius()
  * @return radius
  */
-	/*
-	 * getter getRadius
-	 * return radius, double
-	 */
 	public double getRadius() {
 		return radius;
 	}
-/**
- * setter setRadius
+/*!
+ * setRadius()
  * @param radius
  */
-	/*
-	 * setter setRadius
-	 * param:
-	 * 		radius : double
-	 */
 	public void setRadius(double radius) {
 		this.radius = radius;
 	}
-/**
- * getter getTeta
+/*!
+ * getTeta()
+ * @brief retourne l'angle teta
  * @return teta
  */
-	/*
-	 * getter getTeta
-	 * 
-	 * return teta, double
-	 */
 	public double getTeta() {
 		return teta;
 	}
-/**
- * setter setTeta
+/*!
+ * setTeta()
  * @param teta
  */
-	/*
-	 * setter setTeta
-	 * param:
-	 * 		teta : int
-	 */
 	public void setTeta(double teta) {
 		this.teta = teta;
 	}
