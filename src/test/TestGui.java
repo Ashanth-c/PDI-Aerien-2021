eeb301f023c58cc11acfd05a7f0c5d2812de11ae
Khadija <62354261+AyadiKhadija@users.noreply.github.com>
2021-04-06 13:56:56 +0200

    Update TestGui.java

--- src/test/TestGui.java
+++ src/test/TestGui.java
@@ -4,6 +4,16 @@ import gui.MainGUI;
 
 public class TestGui {
 	
+/*!
+ * @file TestGui.java
+ * @brief tester le gui
+ * @author Khadija
+ * @author Ashanth
+ * @author Maeva
+ * @version 1.0
+ * @date 06/04/2021
+ */
+	
 	public static void main(String[] args) {
 		MainGUI mainGUI = new MainGUI("Aeronef Similator");
 
